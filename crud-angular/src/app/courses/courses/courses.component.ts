c9490fbde878dd05a5a57e6e575551271a700b4a
Mauro <mj.silva7@gmail.com>
2023-06-07 17:56:59 -0300

    Criação de moculos, servico e compartilhamento de cursos

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private courseService: CoursesService) {
+    this.courses = this.courseService.list();
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
